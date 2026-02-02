0fef06dd85e2826a35396988c3584733d270f57f
FarhatCode <farkhatkoshbaev2002@mail.ru>
2026-02-02 15:43:18 +0500

    fix url domen

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['localhost', '127.0.0.1', 'clinic-tooth.ru'],
+    allowedHosts: ['localhost', '127.0.0.1', 'clinic-concept.ru'],
     host: true,
     port: 5173,
   },
